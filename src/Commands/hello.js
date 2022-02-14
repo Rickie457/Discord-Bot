07158fed735f452cef81f0c701068c8904fc1031
Ricky Tran <86625630+Rickie457@users.noreply.github.com>
2022-02-13 22:56:04 -0500

    Delete hello.js

--- src/Commands/hello.js
+++
@@ -1,9 +0,0 @@
-const Command = require("../Structures/Command.js");
-
-module.exports = new Command({
-    name: "Hello",
-    description: "Hello",
-    async run(message, args, client){
-        message.reply("Hi!");
-    }
-});
